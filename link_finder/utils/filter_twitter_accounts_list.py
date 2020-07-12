6cec52e50095cf6b64c2fee99fee5dbd4b2ba9fc
Sailoriae <62501693+Sailoriae@users.noreply.github.com>
2020-07-12 23:41:21 +0200

    Fonction filter_twitter_accounts_list : Mettre en minuscule Parce que Twitter n'est pas sensible à la casse pour le nom d'utilisateur

--- link_finder/utils/filter_twitter_accounts_list.py
+++ link_finder/utils/filter_twitter_accounts_list.py
@@ -2,7 +2,9 @@
 # coding: utf-8
 
 # Liste des comptes Twitter officiels des sites qu'on explore
-OFFICIAL_ACCOUNTS_LIST = [ "DeviantArt" ]
+# METTRE ENTIEREMENT EN MINUSCULE ! Twitter n'est pas sensible à la casse pour
+# les noms d'utilisateurs.
+OFFICIAL_ACCOUNTS_LIST = [ "deviantart" ]
 
 from typing import List
 
@@ -10,10 +12,14 @@ from typing import List
 """
 Permet de filtrer une liste de comptes Twitter.
 Supprime les doublons, et les comptes Twitter des sites qu'on explore.
+
+Met en minuscule pour être certain de supprimer les doublons, car Twitterr
+n'est pas sensible à la casse pour les noms d'utilisateurs.
 """
 def filter_twitter_accounts_list ( accounts_list : List[str] ) -> List[str] :
     return_list : List[str] = []
     for account in accounts_list :
-        if account not in OFFICIAL_ACCOUNTS_LIST and account not in return_list :
-            return_list.append( account )
+        account_lower = account.lower()
+        if account_lower not in OFFICIAL_ACCOUNTS_LIST and account_lower not in return_list :
+            return_list.append( account_lower )
     return return_list
