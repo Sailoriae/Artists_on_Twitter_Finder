298790fcc04d7a722ca975411d8b6090b3645d13
Sailoriae <62501693+Sailoriae@users.noreply.github.com>
2020-08-03 11:59:04 +0200

    Link Finder : Correctif Régex vérification URL

--- server/link_finder/class_Link_Finder.py
+++ server/link_finder/class_Link_Finder.py
@@ -47,7 +47,7 @@ furbooru_url = re.compile(
 # Source :
 # https://stackoverflow.com/questions/3809401/what-is-a-good-regular-expression-to-match-a-url
 url = re.compile(
-    "^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,4}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)$" )
+    "^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,4}\b([-a-zA-Z0-9@:%_\+.~#?&\/\/=]*)$" )
 
 
 """
