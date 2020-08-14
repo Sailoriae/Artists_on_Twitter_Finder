4270d5f4d82768b73df6b13f885dda6b597268fe
Sailoriae <62501693+Sailoriae@users.noreply.github.com>
2020-08-14 09:49:51 +0200

    Amélioration du traitement dans l'étape 2

--- server/app/user_pipeline/thread_step_2_tweets_indexer.py
+++ server/app/user_pipeline/thread_step_2_tweets_indexer.py
@@ -66,92 +66,82 @@ def thread_step_2_tweets_indexer( thread_id : int, shared_memory ) :
             # On passe la requête à l'étape suivante, c'est à dire notre étape
             shared_memory.user_requests.set_request_to_next_step( request )
             
-            # Liste des comptes Twitter dont on doit lancer le scan
-            accounts_to_scan = []
-            
+            # Pour chaque compte de la liste des comptes Twitter trouvé
             for (account_name, account_id) in request.twitter_accounts_with_id :
                 
-                # Si le compte est déjà en cours de scan, on s'y raccroche
-                currently_scanning = shared_memory.scan_requests.get_request( account_id )
-                if currently_scanning != None :
-                    print( "[step_2_th" + str(thread_id) + "] @" + account_name + " est déjà en cours de scan, on le suit !" )
+                # On prend ses deux dernières dates de scan
+                last_scan_1 = bdd_direct_access.get_account_GOT3_last_scan_local_date( account_id )
+                last_scan_2 = bdd_direct_access.get_account_TwitterAPI_last_scan_local_date( account_id )
+                
+                # Si l'une des deux est à NULL, il faut scanner, ou se
+                # rattacher au scan du compte déjà en cours
+                if last_scan_1 == None or last_scan_2 == None :
+                    print( "[step_2_th" + str(thread_id) + "] @" + account_name + " n'est pas dans la base de données ! On lance une nouvelle requête de scan ou on suit celle déjà en cours pour ce compte !" )
                     
-                    # On passe la requête en prioritaire si nécessaire
-                    if not currently_scanning.is_prioritary :
-                        shared_memory.scan_requests.launch_request( account_id,
-                                                                    account_name,
-                                                                    is_prioritary = True )
+                    # launch_request() va soit nous retourner la requête de
+                    # scan en cours, soit en créer une nouvelle
+                    # Si la requête déjà existante n'était pas prioritaire, elle
+                    # va la passer en prioritaire
+                    scan_request = shared_memory.scan_requests.launch_request( account_id,
+                                                                               account_name,
+                                                                               is_prioritary = True )
                     
                     # On suit la progression de cette requête
-                    request.scan_requests += [ currently_scanning._pyroUri ] # Ne peut pas faire de append avec Pyro
+                    request.scan_requests += [ scan_request._pyroUri ] # Ne peut pas faire de append avec Pyro
                 
-                # Sinon, il faut peut-être lancer un scan
+                # Sinon, on prendre la date minimale
                 else :
-                    # Si le compte n'est pas déjà indexé, il faut le scanner
-                    if not bdd_direct_access.is_account_indexed( account_id ) :
-                        print( "[step_2_th" + str(thread_id) + "] @" + account_name + " n'est pas dans la BDD, lancement de son scan !" )
-                        accounts_to_scan.append( (account_name, account_id) )
+                    min_date = min( last_scan_1, last_scan_2 )
                     
-                    # Si le compte est déjà indexé
+                    # On a besoin de mettre le fuseau horaire dans la
+                    # date minimale
+                    min_date = min_date.replace( tzinfo = local_tz )
+                    
+                    # Si cette mise à jour moins 3 jours est plus
+                    # vielle que la date de l'illustration, il faut MàJ
+                    # (On laisse 3 jours de marge à l'artiste pour
+                    # publier sur Twitter)
+                    if min_date - datetime.timedelta( days = 3 ) < request.datetime :
+                        print( "[step_2_th" + str(thread_id) + "] @" + account_name + " est déjà dans la BDD, mais il faut le MàJ car l'illustration de requête est trop récente !" )
+                        
+                        scan_request = shared_memory.scan_requests.launch_request( account_id,
+                                                                               account_name,
+                                                                               is_prioritary = True )
+                        
+                        # On suit la progression de cette requête
+                        request.scan_requests += [ scan_request._pyroUri ] # Ne peut pas faire de append avec Pyro
+                    
                     else :
-                        # On prend ses deux dernières dates de scan
-                        last_scan_1 = bdd_direct_access.get_account_GOT3_last_scan_local_date( account_id )
-                        last_scan_2 = bdd_direct_access.get_account_TwitterAPI_last_scan_local_date( account_id )
-                        
-                        # Si l'une des deux est à NULL, il faut scanner (Mais
-                        # théoriquement le thread d'auto-update l'a fait avant
-                        # nous)
-                        if last_scan_1 == None or last_scan_2 == None :
-                            accounts_to_scan.append( (account_name, account_id) )
-                        
-                        # Sinon, on prendre la date minimale
-                        else :
-                            min_date = min( last_scan_1, last_scan_2 )
-                            
-                            # On a besoin de mettre le fuseau horaire dans la
-                            # date minimale
-                            min_date = min_date.replace( tzinfo = local_tz )
-                            
-                            # Si cette mise à jour moins 3 jours est plus
-                            # vielle que la date de l'illustration, il faut MàJ
-                            # (On laisse 3 jours de marge à l'artiste pour
-                            # publier sur Twitter)
-                            if min_date - datetime.timedelta( days = 3 ) < request.datetime :
-                                print( "[step_2_th" + str(thread_id) + "] @" + account_name + " est déjà dans la BDD, mais il faut le MàJ car l'illustration de requête est trop récente !" )
-                                accounts_to_scan.append( (account_name, account_id) )
-                            else :
-                                print( "[step_2_th" + str(thread_id) + "] @" + account_name + " est déjà dans la BDD, on saute son scan !" )
-            
-            # On lance l'indexation, en mode prioritaire car on est une requête
-            # utilisateur, et on stocke les requêtes qu'on a créé
-            for (account_name, account_id) in accounts_to_scan :
-                request.scan_requests += [
-                    shared_memory.scan_requests.launch_request( account_id,
-                                                                account_name,
-                                                                is_prioritary = True )._pyroUri ] # Ne peut pas faire de append avec Pyro
+                        print( "[step_2_th" + str(thread_id) + "] @" + account_name + " est déjà dans la BDD, et on peut sauter son scan !" )
             
             # Libérer le sémaphore
             shared_memory.user_requests.thread_step_2_tweets_indexer_sem.release()
         
         # Sinon, on vérifie l'état de l'avancement des requêtes qu'on a lancé
-        # Note importante : Nos requêtes ne peuvent pas être annulées car on
-        # est un thread utilisateur
-        # Seules les requêtes non-prioritaires peuvent être annulées
         check_list = []
+        double_continue = False # Pour faire l'équivalent de deux instruction "continue"
         for scan_request_uri in request.scan_requests :
             scan_request = Pyro4.Proxy( scan_request_uri )
             check_list.append( scan_request.finished_GOT3_indexing and scan_request.finished_TwitterAPI_indexing )
             
-            # On vérifie quand même que le scan s'est bien passé, et si un
-            # thread de traitement a planté avec la requête, on l'indique
+            # On vérifie que le scan se passe bien ou s'est bien passé, et si
+            # un thread de traitement a planté avec la requête, on l'indique
             if scan_request.has_failed :
                 request.problem = "PROCESSING_ERROR"
                 
-            # On laisse quand même la requête passer à l'étape 3 de recherche
-            # inversée d'image, au cas où
+                # On abandonne le processus de traitement de cette requête,
+                # même si peut-être qu'on pourrait quand même rechercher
+                shared_memory.user_requests.set_request_to_next_step( request, force_end = True )
+                double_continue = True
+                continue
         
         # Dire qu'on n'est plus en train de traiter cette requête
         shared_memory.user_requests.requests_in_thread.set_request( "thread_step_2_tweets_indexer_number" + str(thread_id), None )
+        
+        # Si l'une des requêtes de scan a eu un problème, on arrête tout avec
+        # cette requête utilisateur
+        if double_continue :
+            continue
         
         # Si toutes nos requêtes ne sont pas finies, on remet la requête en
         # haut de NOTRE file d'attente
