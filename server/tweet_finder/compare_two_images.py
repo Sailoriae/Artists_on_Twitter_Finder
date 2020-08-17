5990e6bd5f173c2386cc6c7fc432cb12a5064767
Sailoriae <62501693+Sailoriae@users.noreply.github.com>
2020-08-17 20:03:30 +0200

    compare_two_images : Gérer les pertes de données chez Twitter

--- server/tweet_finder/compare_two_images.py
+++ server/tweet_finder/compare_two_images.py
@@ -4,15 +4,46 @@
 from PIL import Image
 from io import BytesIO
 import requests
+import urllib
 
 import cv2
 import numpy as np
-from time import time
+from time import sleep, time
 
 try :
     from utils import url_to_cv2_image
 except ModuleNotFoundError : # Si on a été exécuté en temps que module
     from .utils import url_to_cv2_image
+
+
+"""
+Obtenir l'image d'un Tweet, en gérant les erreurs communes qu'on peut
+rencontrer.
+"""
+def get_tweet_image ( url ) :
+    retry_count = 0
+    try :
+        return Image.open(BytesIO(requests.get( url ).content))
+    
+    except urllib.error.HTTPError as error :
+        if error.code == 404 or error.code == 500 or error.code == 403 : # Erreurs insolvables
+            print( "[compare_two_images] Erreur avec l'image de Tweet suivante :", url )
+            print( error )
+            return None
+        else :
+            print( "[compare_two_images] Erreur avec l'image de Tweet suivante :", url )
+            print( error )
+            if error.code == 502 : # Il suffit d'attendre pour ce genre d'erreurs
+                if retry_count < 20 : # Essayer un coup d'attendre
+                    print( "[compare_two_images] On essaye d'attendre 10 secondes..." )
+                    sleep( 10 )
+                    retry_count += 1
+            elif retry_count < 1 : # Essayer un coup d'attendre
+                print( "[compare_two_images] On essaye d'attendre 10 secondes..." )
+                sleep( 10 )
+                retry_count += 1
+            else :
+                raise error
 
 
 """
@@ -23,13 +54,22 @@ Comparer deux images et retourner le pourcentage de similitude.
         plus les images sont proches.
 """
 def compare_two_images ( url1, url2, PRINT_METRICS = True ) :
-    if isinstance( url1, str ) : img1 = Image.open(BytesIO(requests.get( url1 ).content))
-    else : img1 = url1
-    if isinstance( url2, str ) : img2 = Image.open(BytesIO(requests.get( url2 ).content))
-    else : img2 = url2
+    if isinstance( url1, str ) :
+        img1 = get_tweet_image( url1 )
+    else :
+        img1 = url1
+    
+    if isinstance( url2, str ) :
+        img2 = get_tweet_image( url2 )
+    else :
+        img2 = url2
     
     if PRINT_METRICS :
         start = time()
+    
+    # Si l'une des deux images est indisponible, on retourne 0%
+    if img1 == None or img2 == None :
+        return 0
     
     img1 = img1.convert("RGB")
     img2 = img2.convert("RGB")
