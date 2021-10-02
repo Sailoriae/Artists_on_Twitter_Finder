02d5ea771147b9be4d5414185d88cf3da7db4849
Sailoriae <62501693+Sailoriae@users.noreply.github.com>
2021-10-02 15:04:27 +0200

    Noter que l'indexeur vérifie que les Tweets ne sont pas déjà indexés

--- server/tweet_finder/class_Tweets_Lister_with_SearchAPI.py
+++ server/tweet_finder/class_Tweets_Lister_with_SearchAPI.py
@@ -112,6 +112,11 @@ class Tweets_Lister_with_SearchAPI :
             if tweet_dict != None :
                 # Re-filtrer au cas où
                 if int( tweet_dict["user_id"] ) == int ( account_id ) :
+                    # L'ajout dans la file se fait sans vérifier que l'ID du
+                    # Tweet y est déjà présent, parce que ça serait trop long
+                    # (La file peut être très très grande), et que l'indexeur
+                    # vérifie que le Tweet ne soit pas déjà dans la BDD avant
+                    # de l'analyser et de l'indexeur (Voir "Tweet_Indexer")
                     queue.put( tweet_dict )
         
         # Note : Plus besoin de faire de bidouille avec "filter:safe"
