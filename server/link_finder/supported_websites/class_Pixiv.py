46e39f1b4c93c1e91a70a745354d21017d5ea22c
Sailoriae <62501693+Sailoriae@users.noreply.github.com>
2022-03-29 12:01:52 +0200

    Pixiv : Possibilité de passer la position de l'illustration Sur Pixiv, il peut y avoir plusieurs illustrations par page On peut passer la position de l'illustration avec son ID HTML : https://www.pixiv.net/en/artworks/1111#22 Avec : - 1111 : ID de l'illustration (Déjà supporté par AOTF)        - 22 : Position de l'illustration dans la page Exemple : https://www.pixiv.net/en/artworks/87565755#11

--- server/link_finder/supported_websites/class_Pixiv.py
+++ server/link_finder/supported_websites/class_Pixiv.py
@@ -26,9 +26,9 @@ from link_finder.supported_websites.utils.validate_twitter_account_url import va
 
 # ^ = Début de la chaine
 pixiv_artwork_id_regex_new = re.compile(
-    r"^http(?:s)?:\/\/(?:www\.)?pixiv\.net\/(?:en\/)?artworks\/([0-9]+)(?:\/)?" )
+    r"^http(?:s)?:\/\/(?:www\.)?pixiv\.net\/(?:en\/)?artworks\/([0-9]+)(?:#([0-9]+))?(?:\/)?" )
 pixiv_artwork_id_regex_old = re.compile(
-    r"^http(?:s)?:\/\/(?:www\.)?pixiv\.net\/(?:en\/)?member_illust\.php\?(?:[a-zA-Z0-9=_\-&]+)?illust_id=([0-9]+)(?:\/)?" )
+    r"^http(?:s)?:\/\/(?:www\.)?pixiv\.net\/(?:en\/)?member_illust\.php\?(?:[a-zA-Z0-9=_\-&]+)?illust_id=([0-9]+)(?:#([0-9]+))?(?:\/)?" )
 
 """
 Couche d'abstraction à la librairie PixivPy3 pour utiliser l'API publique de
@@ -52,30 +52,38 @@ class Pixiv :
         # à l'API pour une illustration.
         # On met en cache car la demande de l'URL de l'image et la demande des
         # URLs des comptes Twitter sont souvent ensembles.
-        self._cache_illust_url = None
-        self._cache_illust_id = None
-        self._cache_illust_url_json = None
+        self._cache_illust_url : str = None
+        self._cache_illust_id : int = None
+        self._cache_illust_position : int = 1 # Première image par défaut
+        self._cache_illust_url_json : dict = None
     
     """
     Permet d'extraire l'ID de l'URL d'une illustration postée sur Pixiv.
     @param artwork_url L'URL à examiner.
-    @return L'ID de l'illustration postée sur Pixiv.
-            Ou None si ce n'est pas un artwork Pixiv.
+    @return True si l'URL mène à une illustration sur Pixiv.
+            False sinon.
     """
     def _artwork_url_to_id ( self, artwork_url : str ) -> int :
-        result_new = re.match( pixiv_artwork_id_regex_new, artwork_url )
-        result_old = re.match( pixiv_artwork_id_regex_old, artwork_url )
-        if result_new != None :
-            try :
-                return int( result_new.group( 1 ) )
-            except ValueError :
-                return None
-        if result_old != None :
-            try :
-                return int( result_old.group( 1 ) )
-            except ValueError :
-                return None
-        return None
+        # Reset les valeurs qu'on est censé écrire
+        self._cache_illust_id = None
+        self._cache_illust_position = 1
+        
+        result = re.match( pixiv_artwork_id_regex_new, artwork_url )
+        if result == None :
+            result = re.match( pixiv_artwork_id_regex_old, artwork_url )
+        if result == None :
+            return False
+        
+        try :
+            self._cache_illust_id = int( result.group( 1 ) )
+        except ValueError :
+            return False
+        else :
+            if result.group( 2 ) != None :
+                position = int( result.group( 2 ) )
+                if position != 0 and position <= 200 : # Position impossible
+                    self._cache_illust_position = position
+            return True
     
     """
     Mettre en cache le résultat de l'appel à l'API de l'illustration si ce
@@ -83,17 +91,19 @@ class Pixiv :
     Permet de MàJ le cache, si l'URL mène bien vers une illustration.
     
     @param illust_id L'URL de l'illustration Pixiv.
-    @return True si l'URL donné est utilisable.
+    @return True si l'URL mène à une illustration sur Pixiv.
             False sinon.
     """
     def _cache_or_get ( self, illust_url : int ) -> bool :
         if illust_url != self._cache_illust_url :
-            illust_id = self._artwork_url_to_id( illust_url )
+            # Reset les valeurs qu'on est censé écrire
+            self._cache_illust_url = None
+            self._cache_illust_url_json = None
             
-            if illust_id == None :
+            if not self._artwork_url_to_id( illust_url ) :
                 return False
             
-            response = get_with_rate_limits( "https://www.pixiv.net/en/artworks/" + str(illust_id) )
+            response = get_with_rate_limits( "https://www.pixiv.net/en/artworks/" + str(self._cache_illust_id) )
             
             if response.status_code == 404 :
                 return False
@@ -103,7 +113,6 @@ class Pixiv :
             json = json_loads( meta["content"] )
             
             self._cache_illust_url = illust_url
-            self._cache_illust_id = illust_id
             self._cache_illust_url_json = json
         
         return True
@@ -122,24 +131,25 @@ class Pixiv :
         if not self._cache_or_get( illust_url ) :
             return None
         
+        image_dict = None
+        
+        # Sur Pixiv, il peut y avoir plusieurs illustrations sur une même page
+        # On est obligé de refaire un GET pour avoir les URL de toutes les
+        # illustrations sur cette page (Sauf si on veut la première)
+        pages_count = self._cache_illust_url_json["illust"][str(self._cache_illust_id)]["pageCount"]
+        if ( 2 <= self._cache_illust_position and
+             self._cache_illust_position <= pages_count ) :
+            response = get_with_rate_limits( f"https://www.pixiv.net/ajax/illust/{str(self._cache_illust_id)}/pages" )
+            json = response.json()
+            image_dict = json["body"][ self._cache_illust_position - 1 ]
+        
+        # Si on veut la première image, pas besoin de refaire un GET
+        else :
+            image_dict = self._cache_illust_url_json["illust"][str(self._cache_illust_id)]
+        
         # On retourne le résultat voulu
-        # 
-        # Problème avec Pixiv : Il peut y avoir plusieurs illustrations dans
-        # une même page ! C'est chiant. On se contente donc de retourner que la
-        # principale, c'est à dire la première.
-        #
-        # Comme l'utilisateur entre l'URL de la page, et non de l'image, on
-        # devrait donc retourner ici une liste d'URL, et donc scanner plusieurs
-        # images dans notre moteur CBIR.
-        # Ce qui fait que plusieurs tweets seraient retournés, et ça serait à
-        # l'utilisateur de choisir quel est le tweet qui contient l'image
-        # qu'il voulait.
-        # 
-        # Cela serait impossible dans le cas d'un robot qui utilise notre
-        # système !
-        # Il faudrait donc revoir beaucoup de choses...
-        return [ self._cache_illust_url_json["illust"][str(self._cache_illust_id)]["urls"]["original"],
-                 self._cache_illust_url_json["illust"][str(self._cache_illust_id)]["urls"]["regular"] ]
+        return [ image_dict["urls"]["original"],
+                 image_dict["urls"]["regular"] ]
     
     """
     Retourne les noms des comptes Twitter trouvés.
