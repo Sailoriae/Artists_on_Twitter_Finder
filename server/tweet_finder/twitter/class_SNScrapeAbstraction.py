774875084b3324b26ea99c512c6217430e956e77
Sailoriae <62501693+Sailoriae@users.noreply.github.com>
2022-10-23 20:48:47 +0200

    SNScrape peut aussi gérer l'API de timeline On va certainement en avoir besoin, puisque l'API de timeline v1.1 (statuses/user_timeline) ne retourne que le premier média pour les Tweets contenant des images et des vidéos (Possible depuis le 05 octobre 2022). Le problème, c'est que c'est beaucoup plus lent, et en plus on ne peut pas demander à Twitter de nous enlever les retweets (En même temps, c'est une API privée). Donc pour le moment, ce commit ne sert à rien, mais on va devoir l'utiliser. Peut-être qu'à terme, ça serait peut-être intéressant d'utiliser uniquement SNScrape ?

--- server/tweet_finder/twitter/class_SNScrapeAbstraction.py
+++ server/tweet_finder/twitter/class_SNScrapeAbstraction.py
@@ -15,11 +15,12 @@ Modifier un peu SNSCrape pour qu'il fasse exactement ce qu'on veut.
 SNScrape utilise l'API utilisée par l'UI web pour le moteur de recherche :
 https://api.twitter.com/2/search/adaptive.json
 """
-class TwitterSearchScraper ( snscrape.modules.twitter.TwitterSearchScraper ) :
+class TwitterAPIScraper ( snscrape.modules.twitter._TwitterAPIScraper ) :
     def __init__ ( self, *args, **kwargs ) :
         super().__init__(*args, **kwargs)
         
         self._output_function = None
+        self._legacy_version = not hasattr( snscrape.modules.twitter._TwitterAPIScraper, "_make_tweet" )
     
     # Donner en douce à SNScraper le token pour s'authentifier comme le serait
     # un utilisateur sur l'UI web
@@ -38,12 +39,20 @@ class TwitterSearchScraper ( snscrape.modules.twitter.TwitterSearchScraper ) :
     
     # Override : Comme SNScraper ne nous donne pas le JSON original des Tweets,
     # on remplace sa fonction pour le faire nous-même
-    def _tweet_to_tweet ( self, tweet, obj ) :
+    def _make_tweet ( self, *args, **kwargs ) :
         if self._output_function != None :
-            self._output_function( tweet )
+            self._output_function( args[0] )
         
         # On appel quand même sa fonction et on retourne ce qu'elle doit
         # normalement retourner
+        return super()._make_tweet( *args, **kwargs )
+    
+    # Même chose, mais pour les versions antérieures à la 0.4.4 (Non-comprise)
+    def _tweet_to_tweet ( self, tweet, obj ) :
+        if self._legacy_version :
+            if self._output_function != None :
+                self._output_function( tweet )
+        
         return super()._tweet_to_tweet( tweet, obj )
     
     # Setter de la fonction d'output pour l'override juste ci-dessus
@@ -58,6 +67,14 @@ class TwitterSearchScraper ( snscrape.modules.twitter.TwitterSearchScraper ) :
         self._apiHeaders['x-csrf-token'] = self._session.cookies['ct0']
         
         return super()._check_api_response( r )
+
+# API de recherche
+class TwitterSearchScraper ( TwitterAPIScraper, snscrape.modules.twitter.TwitterSearchScraper ) :
+    pass
+
+# API de timeline
+class TwitterProfileScraper ( TwitterAPIScraper, snscrape.modules.twitter.TwitterProfileScraper ) :
+    pass
 
 
 """
@@ -111,3 +128,26 @@ class SNScrapeAbstraction :
         
         # Retourner
         return first_tweet_date, count
+    
+    """
+    Obtenir les Tweets d'un compte avec SNScrape sur l'API utilisée par l'UI
+    web. Cela ne débloque pas la limite des 3.200 Tweets, mais cela permet
+    d'utiliser l'API v2 sans être limité par le "Tweet Cap".
+    C'est une nouvelle limitation qui s'applique à l'application entière (Donc
+    tous ses utilisateurs), et qui tuera les applications tierces comme Twidere
+    le jour où ils arrêteront l'API v1.1 !
+    
+    @param account_id L'ID ou le nom du compte Twitter (SNScrape différencie en
+                      fonction du type, int ou str, y faire attention).
+    @param since_tweet_id ID du Tweet où s'arrêter (Les IDs de Tweets sont par
+                          ordre décroissant lorsqu'on remonte un compte).
+    @param output_function Fonction où mettre les JSON des Tweets.
+    """
+    def user_tweets( self, account_id : int, since_tweet_id : int = None,
+                     output_function = print, RETRIES = 10 ) :
+        scraper = TwitterProfileScraper( account_id, retries = RETRIES )
+        scraper.set_auth_token( self.auth_token )
+        scraper.set_output_function( output_function )
+        for tweet in scraper.get_items() :
+            if since_tweet_id and tweet.id == since_tweet_id :
+                break
