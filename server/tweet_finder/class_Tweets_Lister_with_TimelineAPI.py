caa9cd3c7d48ef462d668f555335949e72263955
Sailoriae <62501693+Sailoriae@users.noreply.github.com>
2022-12-04 20:30:09 +0100

    Listage TimelineAPI : Vérifier que l'ID corresponde au nom du compte On le fait déjà au même endroit dans le listage avec SearchAPI

--- server/tweet_finder/class_Tweets_Lister_with_TimelineAPI.py
+++ server/tweet_finder/class_Tweets_Lister_with_TimelineAPI.py
@@ -143,13 +143,21 @@ class Tweets_Lister_with_TimelineAPI :
             print( f"[List_TimelineAPI] Compte @{account_name} introuvable !" )
             raise Unfound_Account_on_Lister_with_TimelineAPI
         
-        blocks_me = self._twitter.blocks_me( account_id )
+        blocks_me, real_account_name = self._twitter.blocks_me( account_id, append_account_name = True )
         if blocks_me == None :
             print( f"[List_TimelineAPI] Compte @{account_name} introuvable !" )
             raise Unfound_Account_on_Lister_with_TimelineAPI
         if blocks_me :
             print( f"[List_TimelineAPI] Le compte @{account_name} nous bloque, impossible de le scanner !" )
             raise Blocked_by_User_with_TimelineAPI
+        
+        # C'est important avec l'API de recherche, mais pas ici, puisqu'on
+        # utilise uniquement l'ID du compte (Son screen name sert uniquement
+        # aux messages affichés). Mais on le vérifie quand même (Et c'est
+        # symétrique par rapport au listage avec l'API de recherche).
+        if real_account_name != account_name :
+            print( f"[List_TimelineAPI] Le compte ID {account_id} se nomme @{real_account_name} et non @{account_name}." )
+            account_name = real_account_name
         
         if self._DEBUG :
             print( f"[List_TimelineAPI] Listage des Tweets de @{account_name}." )
